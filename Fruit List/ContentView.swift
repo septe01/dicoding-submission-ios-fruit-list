3b037ed46aa7c9673d358c22d4862d5263f74cf7
septpe01 <septe.vhirgo@gmail.com>
2022-06-12 09:13:33 +0700

    initial project

---
+++ Fruit List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruit List
+//
+//  Created by septe habudin on 12/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruit List/Fruit_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_ListApp.swift
+//  Fruit List
+//
+//  Created by septe habudin on 12/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
